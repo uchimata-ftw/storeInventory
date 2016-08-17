98e82ac3996cda4f4e136b7eb53b1bb2f27430ad
uchimata-ftw <ferguson8max@gmail.com>
2016-08-17 16:01:22 -0700

    fixing random stuff

--- inventory.cpp
+++ inventory.cpp
@@ -2,6 +2,7 @@
 Inventory Management Software
 
 Copyright Max Ferguson 2016
+
 This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
